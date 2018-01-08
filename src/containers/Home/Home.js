23276bdaeb8d58ae7f1899dc8eddf7cc65c9d27e
Chase deAnda <Chase deAnda>
2018-01-07 20:11:27 -0600

    Fixed nav links and added buttons to select game mode

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import classes from './Home.scss';
 import AuxComponent from '../../hocs/AuxComponent';
+import { Button } from 'semantic-ui-react';
 
 class Home extends Component {
-    state = {
-        gameMode: {
-
-        }
-    }
     render () {
         return (
             <AuxComponent>
                 <div className={classes.Home}>
-                    <h1>Game Mode Placeholder</h1>
+                    <h1>Select Game Mode</h1>
+                    <Button color="red" size="massive">Classic</Button>
+                    <Button color="red" size="massive">Timed</Button>
+                    <Button color="red" size="massive">Survival</Button>
                 </div>
             </AuxComponent>
 
